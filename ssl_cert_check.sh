77f1486e6676d1e12b0b5fd79a3044f6cd1875f0
Rudimar Remontti <rudiremontti@gmail.com>
2021-06-21 17:06:26 -0300

    Update ssl_cert_check.sh

--- ssl_cert_check.sh
+++ ssl_cert_check.sh
@@ -4,11 +4,11 @@ default_check_timeout=5
 error_code=-65535
 
 function show_help() {
-	echo $error_code
-	# if running on ternimal, show error
-	# without terminal(from zabbix) this will create an unsupported item because return value is stdout + stderr
-	if [ -t 1 ]; then
-	cat >&2 << EOF
+        echo $error_code
+        # if running on ternimal, show error
+        # without terminal(from zabbix) this will create an unsupported item because return value is stdout + stderr
+        if [ -t 1 ]; then
+        cat >&2 << EOF
 
 Usage: $(basename "$0") expire|valid hostname|ip [port[/starttls protocol]] [domain for TLS SNI] [check_timeout]
 
@@ -26,18 +26,18 @@ Output:
 
 * expire:
 
-  * N	number of days left before expiration, 0 or negative if expired
-  * $error_code	failed to get certificate or incorrect parameters
+  * N   number of days left before expiration, 0 or negative if expired
+  * $error_code failed to get certificate or incorrect parameters
 
 * valid:
 
-  * 1	valid
-  * 0	invalid
-  * $error_code	failed to get certificate or incorrect parameters
+  * 1   valid
+  * 0   invalid
+  * $error_code failed to get certificate or incorrect parameters
 
 Return code is always 0, otherwise zabbix agent fails to get item value and triggers would not work. Note: error messages are not printed when running not on a terninal, so that script result from zabbix is always a correct integer.
 EOF
-	fi
+        fi
 
 }
 
@@ -59,21 +59,21 @@ starttls_proto=""
 IFS='/' split=($port)
 
 if [ ${#split[@]} -gt 1 ]; then
-	port="${split[0]}"
-	if [ "${split[1]}" != "tls" ]; then
-		starttls="-starttls"
-		starttls_proto="${split[1]}"
-	fi
+        port="${split[0]}"
+        if [ "${split[1]}" != "tls" ]; then
+                starttls="-starttls"
+                starttls_proto="${split[1]}"
+        fi
 fi
 
 # Check if required utilities exist
 for util in timeout openssl date; do
-	type "$util" >/dev/null || error "Not found in \$PATH: $util"
+        type "$util" >/dev/null || error "Not found in \$PATH: $util"
 done
 
 # Check that busybox date isn't used: it does not support requited date format
 if date --version 2>&1 | grep -qi 'busybox'; then
-	error "Busybox date does not support parsing required date format. date from coreutils package is required"
+        error "Busybox date does not support parsing required date format. date from coreutils package is required"
 fi
 
 # Check arguments
@@ -82,7 +82,7 @@ fi
 [[ "$port" =~ ^[0-9]+$ ]] || error "Port should be a number"
 { [ "$port" -ge 1 ] && [ "$port" -le 65535 ]; } || error "Port should be between 1 and 65535"
 if [ ! -z "$starttls_proto" ]; then
-	[[ "$starttls_proto" =~ ^[a-z0-9]+$ ]] || error "Starttls protocol should be an identifier"
+        [[ "$starttls_proto" =~ ^[a-z0-9]+$ ]] || error "Starttls protocol should be an identifier"
 fi
 [[ "$check_timeout" =~ ^[0-9]+$ ]] || error "Check timeout should be a number"
 
@@ -90,21 +90,21 @@ fi
 if ! output=$( echo \
 | timeout "$check_timeout" openssl s_client $starttls $starttls_proto -servername "$domain" -verify_hostname "$domain" -connect "$host":"$port" 2>/dev/null )
 then
-	error "Failed to get certificate"
+        error "Failed to get certificate"
 fi
 
 # Run checks
 if [ "$check_type" = "expire" ]; then
-	expire_date=$( echo "$output" \
-	| openssl x509 -noout -dates \
-	| grep '^notAfter' | cut -d'=' -f2 )
+        expire_date=$( echo "$output" \
+        | openssl x509 -noout -dates \
+        | grep '^notAfter' | cut -d'=' -f2 )
 
-	expire_date_epoch=$(date -d "$expire_date" +%s) || error "Failed to get expire date"
-	current_date_epoch=$(date +%s)
-	days_left=$(( (expire_date_epoch - current_date_epoch)/(3600*24) ))
-	result "$days_left"
+        expire_date_epoch=$(date -d "$expire_date" +%s) || error "Failed to get expire date"
+        current_date_epoch=$(date +%s)
+        days_left=$(( (expire_date_epoch - current_date_epoch)/(3600*24) ))
+        result "$days_left"
 elif [ "$check_type" = "valid" ]; then
-	# Note: new openssl versions can print multiple return codes for post-handshake session tickets, so we need to get only the first one
-	verify_return_code=$( echo "$output" | grep -E '^ *Verify return code:' | sed -n 1p | sed 's/^ *//' | tr -s ' ' | cut -d' ' -f4 )
-	if [ "$verify_return_code" -eq "0" ]; then result 1; else result 0; fi
+        # Note: new openssl versions can print multiple return codes for post-handshake session tickets, so we need to get only the first one
+        verify_return_code=$( echo "$output" | grep -E '^ *Verify return code:' | sed -n 1p | sed 's/^ *//' | tr -s ' ' | cut -d' ' -f4 )
+        if [ "$verify_return_code" -eq "0" ]; then result 1; else result 0; fi
 fi
